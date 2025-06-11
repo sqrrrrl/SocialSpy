f47a9ea9e03c459ba06b9b6bd86191518eb49b1d
ItzxDwi <107537435+ItzxDwi@users.noreply.github.com>
2025-06-11 23:35:46 +0800

    optimize code

--- src/SocialSpy/sqrrl/WebhookTask.php
+++ src/SocialSpy/sqrrl/WebhookTask.php
@@ -3,23 +3,25 @@
 namespace SocialSpy\sqrrl;
 
 use pocketmine\scheduler\AsyncTask;
+use pocketmine\utils\Internet;
+use pocketmine\Server;
+
+use function igbinary_unserialize;
 
 class WebhookTask extends AsyncTask{
 
-    private string $webhook_url, $webhook_content;
-
-    public function __construct($webhook_url, $webhook_content){
-        $this->webhook_url = $webhook_url;
-        $this->webhook_content = $webhook_content;
-    }
+    public function __construct(private string $webhook_url, private string $webhook_content) {}
 
     public function onRun(): void {
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $this->webhook_url);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode(unserialize($this->webhook_content)));
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_exec($curl);
+        $webhookError = null;
+        Internet::postURL($this->webhook_url, json_encode(igbinary_unserialize($this->webhook_content)), 10, ['Content-Type: application/json'], $err);
+        $this->setResult($webhookError);
+    }
+    
+    public function onCompletion(): void {
+        $webhookError = $this->getResult();
+        if ($webhookError !== null) {
+          Server::getInstance()->getLogger()->error("[SocialSpy] An error occurred while posting webhook: $webhookError");
+        }
     }
 }
