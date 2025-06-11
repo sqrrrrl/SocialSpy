12786581fd16400b5ffc26ad97166be780076ae8
ItzxDwi <107537435+ItzxDwi@users.noreply.github.com>
2025-06-11 23:35:17 +0800

    optimize code

--- src/SocialSpy/sqrrl/EventListener.php
+++ src/SocialSpy/sqrrl/EventListener.php
@@ -5,18 +5,19 @@ namespace SocialSpy\sqrrl;
 use pocketmine\console\ConsoleCommandSender;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 use pocketmine\event\server\CommandEvent;
 
-use function in_array;
+use function igbinary_serialize;
 
 class EventListener implements Listener {
 
+    public function __construct(private Main $plugin) {}
+
     public function onLeave(PlayerQuitEvent $event){
         $name = $event->getPlayer()->getName();
-        if (in_array($name, Main::$SocialSpy)){
-            unset(Main::$SocialSpy[array_search($name, Main::$SocialSpy)]);
+        if ($this->plugin->isSocialSpyEnabled($name)){
+            $this->plugin->disableSocialSpy($name);
         }
     }
 
@@ -25,24 +26,25 @@ class EventListener implements Listener {
      * @priority MONITOR
      */
     public function onPlayerCommand(CommandEvent $event) {
-        $console = new ConsoleCommandSender(Main::getMain()->getServer(), Main::getMain()->getServer()->getLanguage());
+        $server = $this->plugin->getServer();
+        $console = new ConsoleCommandSender($server, $server->getLanguage());
         $command = $event->getCommand();
-        $player = $event->getSender();
-        $console->sendMessage('§9SocialSpy §6»§r ' . TextFormat::GRAY . TextFormat::ITALIC . $player->getName() . TextFormat::RESET . ": " . TextFormat::AQUA . "/" . $command);
-        if (Main::getMain()->getConfig()->get("webhook") == "on"){
-            if (!$player->hasPermission("socialspy.hide")) {
-                $webhook_url = Main::getMain()->getConfig()->get("url");
+        $sender = $event->getSender();
+        $console->sendMessage('§9SocialSpy §6»§r ' . TextFormat::GRAY . TextFormat::ITALIC . $sender->getName() . TextFormat::RESET . ": " . TextFormat::AQUA . "/" . $command);
+        if ($this->plugin->getConfig()->get("webhook") == "on"){
+            if (!$sender->hasPermission("socialspy.hide")) {
+                $webhook_url = $this->plugin->getConfig()->get("url");
                 $webhook_content = [
-                    "content" => $player->getName() . ": /" . $command,
+                    "content" => $sender->getName() . ": /" . $command,
                     "username" => "SocialSpy"
                 ];
-                Main::getMain()->getServer()->getAsyncPool()->submitTask(new WebhookTask($webhook_url, serialize($webhook_content)));
+                $server->getAsyncPool()->submitTask(new WebhookTask($webhook_url, igbinary_serialize($webhook_content)));
             }
         }
-        foreach (Server::getInstance()->getOnlinePlayers() as $p) {
-            if (in_array($p->getName(), Main::$SocialSpy)) {
-                if (!$player->hasPermission("socialspy.hide")) {
-                    $p->sendMessage('§9SocialSpy §6»§r ' . TextFormat::GRAY . TextFormat::ITALIC . $player->getName() . TextFormat::RESET . ": " . TextFormat::AQUA . "/" . $command);
+        foreach ($server->getOnlinePlayers() as $player) {
+            if ($this->plugin->isSocialSpyEnabled($player->getName())) {
+                if (!$sender->hasPermission("socialspy.hide")) {
+                    $player->sendMessage('§9SocialSpy §6»§r ' . TextFormat::GRAY . TextFormat::ITALIC . $sender->getName() . TextFormat::RESET . ": " . TextFormat::AQUA . "/" . $command);
                 }
             }
         }
